839a39bdcec77f4efad2ff69d316bad71c778678
VictorFerreiraS <victor.fagundes586@gmail.com>
2023-04-14 15:38:48 -0300

    This is prototype 1

--- src/signin/SignIn.js
+++ src/signin/SignIn.js
@@ -1,135 +1,99 @@
-import React from 'react';
-import Avatar from "@mui/material/Avatar";
+import * as React from "react";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { Paper } from "@mui/material";
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
 
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://resume-586.web.app/">
-        Victor Fagundes
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
-const theme = createTheme();
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+  const initialValues = {
+    email: "",
+    password: "",
   };
 
+  const validationSchema = Yup.object({
+    email: Yup.string().required("Required").email("invalid email address"),
+    password: Yup.string().required("Required"),
+  });
+
+  const onSubmit = (values, { resetForm, setSubmitting }) => {};
+
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundImage:
-          'url("https://images.pexels.com/photos/5851791/pexels-photo-5851791.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        height: "100%",
-      }}
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
     >
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Paper
+      {({ isSubmitting }) => (
+        <Box
           sx={{
-            padding: "20px",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
+            backgroundImage:
+              "radial-gradient( circle farthest-corner at 10% 20%,  rgba(100,43,115,1) 0%, rgba(4,0,4,1) 90% )",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+          <Container
+            maxWidth="xs"
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <NavLink to="signup">
-                  <Link variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </NavLink>
-              </Grid>
-            </Grid>
-          </Box>
-        </Paper>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </Box>
+            <Paper>
+              <Form>
+                <Grid padding={"20px"} container spacing={2}>
+                  <Grid item xs={12} id={"alignCenter"}>
+                    <Typography variant={"h5"}> Sign In</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field name={"email"}>
+                      {({ field }) => (
+                        <TextField
+                          label="Email"
+                          variant="outlined"
+                          fullWidth
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage name={"email"} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field name={"password"}>
+                      {({ field }) => (
+                        <TextField
+                          label="Password"
+                          variant="outlined"
+                          fullWidth
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage name={"password"} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                      disabled={isSubmitting}
+                    >
+                      Submit
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Paper>
+          </Container>
+        </Box>
+      )}
+    </Formik>
   );
 }
--- src/signup/SignUp.js
+++ src/signup/SignUp.js
@@ -1,49 +1,39 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import VisibilityIcon from "@mui/icons-material/Visibility";
-import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { NavLink } from "react-router-dom";
-import { IconButton, InputAdornment, Paper } from "@mui/material";
+import { Paper } from "@mui/material";
 import axios from "axios";
-import { useState } from "react";
 
-const theme = createTheme();
-
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
 
 export default function SignUp() {
   const [responseStatus, setResponseStatus] = React.useState();
-  const [showPassword, setShowPassword] = useState(false);
-  
-  const handleClickShowPassword = () => {
-    setShowPassword(!showPassword);
+
+  const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const registrationData = {
-      firstName: data.get("firstName"),
-      lastName: data.get("lastName"),
-      email: data.get("email"),
-      password: data.get("password"),
-    };
+  const validationSchema = Yup.object({
+    firstName: Yup.string().required("Required"),
+    lastName: Yup.string().required("Required"),
+    email: Yup.string().required("Required").email("invalid email address"),
 
-    console.log(registrationData);
+    password: Yup.string()
+      .required("Required")
+      .min(8, "Password must be at least 8 characters "),
+  });
 
+  const onSubmit = (values, { resetForm, setSubmitting }) => {
     axios
-      .post(
-        "http://localhost:8080/api/v1/users/register-user",
-        registrationData
-      )
+      .post("http://localhost:8080/api/v1/users/register-user", values)
       .then((response) => {
         console.log(response);
         if (response.status === 200) {
@@ -53,131 +43,111 @@ export default function SignUp() {
         }
       })
       .catch((error) => console.log(error));
+
+    // Reset the form and set isSubmitting to false
+    resetForm();
+    setSubmitting(false);
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundImage:
-            'url("https://images.pexels.com/photos/5851791/pexels-photo-5851791.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          height: "100%",
-        }}
-      >
-        <Container maxWidth="xs">
-          <Paper
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {({ isSubmitting }) => (
+        <Box
+          sx={{
+            backgroundImage:
+              "radial-gradient( circle farthest-corner at 10% 20%,  rgba(100,43,115,1) 0%, rgba(4,0,4,1) 90% )",
+          }}
+        >
+          <Container
+            maxWidth="xs"
             sx={{
-              padding: "20px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
             }}
           >
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
-              <Typography component="h1" variant="h5">
-                Sign up
-              </Typography>
-              <Box
-                component="form"
-                noValidate
-                onSubmit={handleSubmit}
-                sx={{ mt: 3 }}
-              >
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      autoComplete="given-name"
-                      name="firstName"
-                      required
-                      fullWidth
-                      id="firstName"
-                      label="First Name"
-                      autoFocus
-                    />
+            <Paper>
+              <Form>
+                <Grid padding={"20px"} container spacing={2}>
+                  <Grid item xs={12} id={"alignCenter"}>
+                    <Typography variant={"h5"}> Sign Up</Typography>
                   </Grid>
-                  <Grid item xs={12} sm={6}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="lastName"
-                      label="Last Name"
-                      name="lastName"
-                      autoComplete="family-name"
-                    />
+                  <Grid item xs={6}>
+                    <Field name={"firstName"}>
+                      {({ field }) => (
+                        <TextField
+                          label="First Name"
+                          variant="outlined"
+                          fullWidth
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage name={"firstName"} />
                   </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      required
-                      fullWidth
-                      id="email"
-                      label="Email Address"
-                      name="email"
-                      autoComplete="email"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      id="password"
-                      name="password"
-                      fullWidth
-                      type={showPassword ? "text" : "password"}
-                      label="Password"
-                      required
-                      InputProps={{
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <IconButton
-                              aria-label="toggle password visibility"
-                              onClick={handleClickShowPassword}
-                              edge="end"
-                            >
-                              {showPassword ? (
-                                <VisibilityIcon />
-                              ) : (
-                                <VisibilityOffIcon />
-                              )}
-                            </IconButton>
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                  </Grid>
-                </Grid>
 
-                <Button
-                  type="submit"
-                  fullWidth
-                  color={responseStatus ? "success" : "primary"}
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2 }}
-                >
-                  {responseStatus ? "success" : "Sing Up"}
-                </Button>
-                <Grid container justifyContent="flex-end">
-                  <Grid item>
-                    <NavLink to="/">
-                      <Link href="#" variant="body2">
-                        Already have an account? Sign in
-                      </Link>
-                    </NavLink>
+                  <Grid item xs={6}>
+                    <Field name={"lastName"}>
+                      {({ field }) => (
+                        <TextField
+                          label="Last Name"
+                          variant="outlined"
+                          fullWidth
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage name={"lastName"} />
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Field name={"email"}>
+                      {({ field }) => (
+                        <TextField
+                          label="Email"
+                          variant="outlined"
+                          fullWidth
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage name={"email"} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field name={"password"}>
+                      {({ field }) => (
+                        <TextField
+                          label="Password"
+                          variant="outlined"
+                          fullWidth
+                          {...field}
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage name={"password"} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      fullWidth
+                      disabled={isSubmitting}
+                    >
+                      Submit
+                    </Button>
                   </Grid>
                 </Grid>
-              </Box>
-            </Box>
-          </Paper>
-        </Container>
-      </Box>
-    </ThemeProvider>
+              </Form>
+            </Paper>
+          </Container>
+        </Box>
+      )}
+    </Formik>
   );
 }
