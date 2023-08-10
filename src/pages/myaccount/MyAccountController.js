d3f4859d8262b2a3707bd8fc9f4f0eec56b5ccf1
victorferreiras <victor.fagundes586@gmail.com>
2023-08-10 16:01:48 -0300

    send confirmation email implemented

--- src/pages/myaccount/MyAccountController.js
+++ src/pages/myaccount/MyAccountController.js
@@ -1,16 +1,66 @@
 import React, { useEffect, useState } from "react";
 import MyAccountView from "./MyAccountView";
 import jwt_decode from "jwt-decode";
-import {getUserInfoFromToken} from "../../utils/utils.js";
+import { getUserInfoFromToken } from "../../utils/utils.js";
+import { useNavigate } from "react-router-dom";
 
 export default function MyAccountController() {
+  const navigate = useNavigate();
   const userInfo = getUserInfoFromToken();
   const [firstName, setFirstName] = useState(userInfo.firstName);
   const [lastName, setLastName] = useState(userInfo.lastName);
+  const [isUserConfirmed, setIsUserConfirmed] = useState(userInfo.confirmed);
+
+  const [deleteUserResponse, setDeleteUserResponse] = useState("");
+  const [
+    emailConfirmationRequestResponse,
+    setEmailConfirmationRequestResponse,
+  ] = useState("Send Confirmation email");
+
+  const deleteUser = async () => {
+    return await fetch("http://localhost:8080/api/v1/user/delete-user", {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    }).then((response) => {
+      setDeleteUserResponse(response.text);
+      setTimeout(() => {
+        navigate("/");
+      }, 500);
+    });
+  };
+
+  const sendConfirmationEmail = async () => {
+    setEmailConfirmationRequestResponse("Loading...");
+
+    try {
+      const response = await fetch(
+        "http://localhost:8080/api/v1/user/send-confirmation-email",
+        {
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        }
+      );
+
+      console.log(response);
+      if (response.status === 200) {
+        setEmailConfirmationRequestResponse("Email Sent");
+      } else {
+        setEmailConfirmationRequestResponse("Error sending email");
+      }
+    } catch (error) {
+      setEmailConfirmationRequestResponse("Error: " + error.message);
+    }
+  };
 
   return (
     <>
-      <MyAccountView firstName={firstName} lastName={lastName} />
+      <MyAccountView
+        firstName={firstName}
+        lastName={lastName}
+        deleteUser={deleteUser}
+        isUserConfirmed={isUserConfirmed}
+        deleteUserResponse={deleteUserResponse}
+        emailConfirmationRequestResponse={emailConfirmationRequestResponse}
+        sendConfirmationEmail={sendConfirmationEmail}
+      />
     </>
   );
 }
--- src/pages/myaccount/MyAccountView.js
+++ src/pages/myaccount/MyAccountView.js
@@ -1,88 +1,108 @@
 import {
-  Box,
-  Collapse,
-  Container,
-  Grid,
-  List,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  ListSubheader,
-  Paper,
-  Typography,
+    Box,
+    Collapse,
+    Container,
+    Grid,
+    List,
+    ListItemButton,
+    ListItemIcon,
+    ListItemText,
+    ListSubheader,
+    Paper,
+    Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import SecurityIcon from "@mui/icons-material/Security";
 import DeleteIcon from "@mui/icons-material/Delete";
 import PasswordIcon from "@mui/icons-material/Password";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
-import { useSelector } from "react-redux";
+import MarkEmailReadIcon from "@mui/icons-material/MarkEmailRead";
 import useDocumentTitle from "../../hooks/useDocumentTitle";
 import "./myaccount.css";
 
-export default function MyAccountView({ firstName, lastName }) {
-  useDocumentTitle("Account Settings");
-  const [open, setOpen] = useState(false);
+export default function MyAccountView({
+                                          firstName,
+                                          lastName,
+                                          deleteUser,
+                                          isUserConfirmed,
+                                          sendConfirmationEmail,
+                                          emailConfirmationRequestResponse,
+                                      }) {
+    useDocumentTitle("Account Settings");
+    const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
+    const handleClick = () => {
+        setOpen(!open);
+    };
 
-  return (
-    <Box className="myaccount-box-wrapper">
-      <Container maxWidth={"sm"}>
-        <Paper className="myaccount-form-wrapper">
-          <Grid container>
-            <Grid item xs={12}>
-              <Typography variant="h5">
-                Hello
-                {" " + firstName + " " + lastName}
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              <List
-                subheader={
-                  <ListSubheader component="" id="nested-list-subheader">
-                    Account Settings
-                  </ListSubheader>
-                }
-              >
-                <ListItemButton>
-                  <ListItemIcon>
-                    <NotificationsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Notifications" />
-                </ListItemButton>
-                <ListItemButton onClick={handleClick}>
-                  <ListItemIcon>
-                    <SecurityIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Security" />
-                  {open ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
-                <Collapse in={open} timeout="auto" unmountOnExit>
-                  <List component="div" disablePadding>
-                    <ListItemButton sx={{ pl: 4 }}>
-                      <ListItemIcon>
-                        <PasswordIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Change Password" />
-                    </ListItemButton>
-                    <ListItemButton sx={{ pl: 4 }}>
-                      <ListItemIcon>
-                        <DeleteIcon />
-                      </ListItemIcon>
-                      <ListItemText primary="Delete Account" />
-                    </ListItemButton>
-                  </List>
-                </Collapse>
-              </List>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Container>
-    </Box>
-  );
+    return (
+        <Box className="myaccount-box-wrapper">
+            <Container maxWidth={"sm"}>
+                <Paper className="myaccount-form-wrapper">
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Typography variant="h5">
+                                Hello
+                                {" " + firstName + " " + lastName}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <List
+                                subheader={
+                                    <ListSubheader component="" id="nested-list-subheader">
+                                        Account Settings
+                                    </ListSubheader>
+                                }
+                            >
+                                <ListItemButton>
+                                    <ListItemIcon>
+                                        <NotificationsIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Notifications"/>
+                                </ListItemButton>
+                                <ListItemButton onClick={handleClick}>
+                                    <ListItemIcon>
+                                        <SecurityIcon/>
+                                    </ListItemIcon>
+                                    <ListItemText primary="Security"/>
+                                    {open ? <ExpandLess/> : <ExpandMore/>}
+                                </ListItemButton>
+                                <Collapse in={open} timeout="auto" unmountOnExit>
+                                    <List component="div" disablePadding>
+                                        <ListItemButton sx={{pl: 4}}>
+                                            <ListItemIcon>
+                                                <PasswordIcon/>
+                                            </ListItemIcon>
+                                            <ListItemText primary="Change Password"/>
+                                        </ListItemButton>
+                                        {isUserConfirmed ? (
+                                            ""
+                                        ) : (
+                                            <ListItemButton
+                                                onClick={() => sendConfirmationEmail()}
+                                                sx={{pl: 4}}
+                                            >
+                                                <ListItemIcon>
+                                                    <MarkEmailReadIcon/>
+                                                </ListItemIcon>
+                                                <ListItemText primary={emailConfirmationRequestResponse}/>
+                                            </ListItemButton>
+                                        )}
+                                        <ListItemButton onClick={() => deleteUser()} sx={{pl: 4}}>
+                                            <ListItemIcon>
+                                                <DeleteIcon/>
+                                            </ListItemIcon>
+                                            <ListItemText primary="Delete Account"/>
+                                        </ListItemButton>
+                                    </List>
+                                </Collapse>
+                            </List>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Container>
+        </Box>
+    );
 }
